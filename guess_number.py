736676e1375fdca4d08c2bc8f93f6da87c96eb95
Alex Pavicic <alexpavicicic@gmail.com>
2024-03-14 16:40:12 -0500

    Added too high contional if guess is over, too low conditional if guess is under

--- guess_number.py
+++ guess_number.py
@@ -10,6 +10,10 @@ while guess != "q" and guessed != True:
     elif int(guess) == number:
         print("Congratulations! You guessed the right number.")
         guessed = True
+    elif int(guess) > number:
+        print(f"Sorry! Your guess is too high, Try Again! Press q to quit")
+    elif int(guess) < number:
+        print(f"Sorry! Your guess is too low, Try Again! Press q to quit")
     else:
-        print(f"Sorry! Try Again!, q to quit")
+        print(f"Sorry! Try Again! Press q to quit")
     
